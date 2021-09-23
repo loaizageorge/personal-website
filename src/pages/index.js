64911e2cea720a4a1d4aff2638ec092efa8f3220
George Loaiza <george.loaiza@mjfreeway.com>
2021-09-22 23:32:12 -0400

    Add global styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Portfolio from "../components/Portfolio";
+import "../styles/styles.css";
 
 
 // data
